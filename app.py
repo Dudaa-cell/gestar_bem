061b77bc18dc58a9efa0e18539c421e17da278cf
IsabelaWisses <wissesisabela@gmail.com>
2026-01-22 21:35:08 -0300

    Envio da estrutura inicial do projeto web

---
+++ app.py
@@ -0,0 +1,6 @@
+# app.py será onde iremos colocar o backend do python.
+# templates será onde colocaremos todos os arquivos .html ("Frontend") - Um para cada página do projeto
+# static será onde colocaremos todas as imagens -> img: Terá as imagens -> js: Todo e qualquer script js e a conexão com back -> css: Estilização
+# index -> site de abertura
+# login
+# home -> página principal do sistema
